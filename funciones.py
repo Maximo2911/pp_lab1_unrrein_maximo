92d2c7e76f94398399cb745b17b98cc729b81937
Maximo Unrrein <maximounrrein@gmail.com>
2023-05-25 15:43:29 -0300

    primera carga

---
+++ funciones.py
@@ -0,0 +1,17 @@
+import json
+import os
+link = "W:\segundo_parcial\pp_lab1_unrrein_maximo\dt.json"
+#punto 1 
+def mostrar_nombre_posicion():
+    '''
+    Esta funcion muestra los jugadores y su posicion
+    Toma como dato la lista con los jugadores
+    Retorna un los jugadores con su respectiva posicion
+    '''
+    return os.getcwd()
+#     with open("dt.json", "r") as archivo:
+#         # json.read(10)
+#         contenido = archivo.read()
+#         lista_archivo = json.loads(contenido)
+#     print(lista_archivo)
+print(mostrar_nombre_posicion())
